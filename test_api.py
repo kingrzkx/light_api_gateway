2fc4348aa31a07c80e251924914c2c2da56264a8
Star <2042288683@qq.com>
2026-09-24 15:40:22 +0800

    v1

--- test_api.py
+++ test_api.py
@@ -2,7 +2,7 @@ import requests
 
 url = "http://127.0.0.1:8000/api/demo"
 headers = {
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0X3VzZXIwMSIsImV4cCI6MTc4ODE5NTY0NX0.5FWLsnZHx2seDw4B78lQDjeVvTkjN76ZQN5AgHt-MH0"
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0X3VzZXIwMSIsImV4cCI6MTc4ODI0MDkwM30.9gCjv75sz3dMUz6FeVIgQIhqJLZvDIF1kvY70s7-sN4"
 }
 
 resp = requests.get(url, headers=headers)
